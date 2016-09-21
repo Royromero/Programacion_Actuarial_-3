cd568afbfd7f7ace3b9733b84d75dcb7580268f4
Rodrigo Romero <royromerog@gmail.com>
2016-09-21 02:38:53 -0500

    Caso no. 1
    análisis de medias  y casos completos de un data

---
+++ completos.R
@@ -0,0 +1,30 @@
+completos <- function(directorio ="~/Rodrigo/GitHub/Programacion_Actuarial_-3/specdata", id=1:332){
+  setwd(directorio)
+  sandia <- vector("numeric")
+  
+for(i in id) {
+  #Lectura de Datos
+  if (i<10) {
+    tabla <- read.csv(paste("00", i, ".csv", sep=""))
+  }else if(i>=10 && i<100 ) {
+    tabla<-read.csv(paste("0", i,".csv", sep=""))
+  }else {
+    tabla<-read.csv(paste(i,".csv",sep=""))
+  }
+  
+  #Casos completos
+  buenos <- complete.cases(tabla)
+  kim <- tabla[buenos, ]
+  nf <- nrow(kim)
+  sandia <- c(sandia,nf)
+}
+  
+  table1 <- data.frame(id,nobs=sandia)
+  table1
+}
+completos(,c(2,4,8,10,12))
+
+
+
+
+
